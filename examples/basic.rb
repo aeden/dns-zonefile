71554358bfeabdd154bb8fd2bc0c3582cb41503b
Anthony Eden <me@anthonyeden.com>
2016-12-26 14:37:56 +0100

    Example of setting the origin when parsing.

--- examples/basic.rb
+++ examples/basic.rb
@@ -5,8 +5,7 @@
 require 'dns/zonefile'
 
 zonefile = "example.com.zonefile"
-zone_string = File.read(zonefile)
-zone = DNS::Zonefile.parse(zone_string)
+zone = DNS::Zonefile.parse(File.read(zonefile))
 
 puts zone.soa.origin.to_s
 puts zone.soa.ns.to_s
---
+++ examples/basic_origin.rb
@@ -0,0 +1,15 @@
+# First make sure you have installed the `dns-zonefile` gem.
+# 
+# Run this script with `ruby basic.rb`
+
+require 'dns/zonefile'
+
+zonefile = "example.com.zonefile"
+zone = DNS::Zonefile.load(File.read(zonefile), 'example.com.')
+
+puts zone.soa.origin
+puts zone.soa.nameserver
+# get all MX records
+zone.records_of(DNS::Zonefile::MX).each do |rec|
+  puts "#{rec.host} #{rec.klass} #{rec.ttl} #{rec.priority} #{rec.domainname}"
+end
