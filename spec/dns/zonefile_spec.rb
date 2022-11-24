ed72160b1380ed6492b594fd91afbb323383dc3b
Anthony Eden <me@anthonyeden.com>
2022-11-24 09:57:53 +0100

    Allow reordering ot class and TTL for TXT records

--- spec/dns/zonefile_spec.rb
+++ spec/dns/zonefile_spec.rb
@@ -103,6 +103,11 @@ RSpec.describe "DNS::Zonefile" do
         with LF and CRLF line endings"
         
         with-underscore TXT abc_123
+
+        with-ttl 60 TXT "example"
+        with-class IN TXT "example"
+        ttl-class 60 IN TXT "example"
+        class-ttl IN 60 TXT "example"
         
         @             CAA   0 issue "letsencrypt.org"
         issuewild     CAA   0 issuewild "comodoca.com"
@@ -183,7 +188,7 @@ RSpec.describe "DNS::Zonefile" do
 
     it "should build the correct number of resource records" do
       zone = DNS::Zonefile.parse(@zonefile)
-      expect(zone.rr.size).to eq(88)
+      expect(zone.rr.size).to eq(92)
     end
 
     it "should build the correct NS records" do
@@ -408,7 +413,7 @@ RSpec.describe "DNS::Zonefile" do
     it "should build the correct TXT records" do
       zone = DNS::Zonefile.load(@zonefile)
       txt_records = zone.records_of DNS::Zonefile::TXT
-      expect(txt_records.size).to eq(8)
+      expect(txt_records.size).to eq(12)
 
       expect(txt_records.detect { |r|
         r.host == "_domainkey.example.com." && r.data == '"v=DKIM1\;g=*\;k=rsa\; p=4tkw1bbkfa0ahfjgnbewr2ttkvahvfmfizowl9s4g0h28io76ndow25snl9iumpcv0jwxr2k"'
